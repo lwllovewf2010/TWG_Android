546213d842482428f43da568c64a981c850aa228
Art <artem@wellnuts.cz>
2014-03-28 15:28:08 +0300

    Отмечание пункта меню для текущего экрана исправлено для работы с API<11

---
+++ src/com/modusgo/ubi/customviews/CheckedTypefacedTextView.java
@@ -0,0 +1,32 @@
+package com.modusgo.ubi.customviews;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.CheckedTextView;
+
+import com.modusgo.ubi.R;
+
+
+public class CheckedTypefacedTextView extends CheckedTextView {
+
+	public CheckedTypefacedTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        //Typeface.createFromAsset doesn't work in the layout editor. Skipping...
+        if (isInEditMode()) {
+            return;
+        }
+
+        TypedArray styledAttrs = context.obtainStyledAttributes(attrs, R.styleable.TypefacedTextView);
+        String fontName = styledAttrs.getString(R.styleable.TypefacedTextView_typeface);
+        styledAttrs.recycle();
+
+        if (fontName != null) {
+            Typeface typeface = Typeface.createFromAsset(context.getAssets(), "fonts/"+fontName);
+            setTypeface(typeface);
+        }
+    }
+	
+}
