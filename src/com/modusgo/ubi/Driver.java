133fa0d04a1da62cf4068d39b277773a4bf9454b
Art <artem@wellnuts.cz>
2014-07-29 17:40:34 +0300

    Home screen drivers list added

--- src/com/modusgo/ubi/Driver.java
+++ src/com/modusgo/ubi/Driver.java
@@ -6,19 +6,25 @@ public class Driver implements Serializable{
 	
 	private static final long serialVersionUID = 1315237349232671000L;
 	public String name;
+	public int imageId;
 	public String vehicle;
 	public String lastTripDate;
 	public String score;
+	public boolean diagnosticsOK;
+	public boolean alertsOK;
 	
 	public Driver(String name, String score){
 		this.name = name;
 		this.score = score;
 	}
 	
-	public Driver(String name, String vehicle, String lastTripDate){
+	public Driver(String name, int imageId, String vehicle, String lastTripDate, boolean diagnosticsOK, boolean alertsOK){
 		this.name = name;
+		this.imageId = imageId;
 		this.vehicle = vehicle;
 		this.lastTripDate = lastTripDate;
+		this.diagnosticsOK = diagnosticsOK;
+		this.alertsOK = alertsOK;
 	}
 
 }
--- src/com/modusgo/ubi/HomeActivity.java
+++ src/com/modusgo/ubi/HomeActivity.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,12 +22,14 @@ public class HomeActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 		
+		getActionBar().setTitle("HOME");
+		
 		ListView lvDrivers = (ListView)findViewById(R.id.listViewDrivers);
 		
 		ArrayList<Driver> data = new ArrayList<Driver>();
-		data.add(new Driver("Melissa\nHasalonglastname", "2012 Ford Edge","07/05/2014 05:00 PM PST"));
-		data.add(new Driver("Melissa\nHasalonglastname", "2011 Ford Focus","07/05/2014 05:00 PM PST"));
-		data.add(new Driver("Melissa\nHasalonglastname", "2010 Ford Fiesta","07/05/2014 05:00 PM PST"));
+		data.add(new Driver("Melissa Hasalonglastname", R.drawable.person_test, "2012 Ford Edge","07/05/2014 05:00 PM PST", true, true));
+		data.add(new Driver("Diana Johnson", R.drawable.person_test2, "2011 Ford Focus","07/05/2014 05:00 PM PST", true, false));
+		data.add(new Driver("Kate Summerton", R.drawable.person_test3, "1967 Ford Mustang","07/05/2014 05:00 PM PST", false, true));
 		
 		DriversAdapter driversAdapter = new DriversAdapter(this, data);
 		
@@ -60,19 +65,32 @@ public class HomeActivity extends Activity{
 		}
 
 		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
+		public View getView(final int position, View convertView, ViewGroup parent) {
 			// используем созданные, но не используемые view
 		    View view = convertView;
 		    if (view == null) {
 		      view = lInflater.inflate(R.layout.driver_item, parent, false);
 		    }
 
-		    Driver p = getDriver(position);
+		    Driver d = getDriver(position);
 
-		    ((TextView) view.findViewById(R.id.tvName)).setText(p.name);
-		    ((TextView) view.findViewById(R.id.tvVehicle)).setText(p.vehicle);
-		    ((TextView) view.findViewById(R.id.tvDate)).setText(p.lastTripDate);
-
+		    ((TextView) view.findViewById(R.id.tvName)).setText(d.name);
+		    ((TextView) view.findViewById(R.id.tvVehicle)).setText(d.vehicle);
+		    ((TextView) view.findViewById(R.id.tvDate)).setText(d.lastTripDate);
+		    
+		    ((ImageView)view.findViewById(R.id.imagePhoto)).setImageResource(d.imageId);
+		    
+		    if(d.diagnosticsOK){
+		    	((ImageView)view.findViewById(R.id.imageDiagnostics)).setImageResource(R.drawable.ic_diagnostics_green);
+		    }else{
+		    	((ImageView)view.findViewById(R.id.imageDiagnostics)).setImageResource(R.drawable.ic_diagnostics_red);		    	
+		    }
+		    
+		    if(d.alertsOK){
+		    	((ImageView)view.findViewById(R.id.imageAlerts)).setImageResource(R.drawable.ic_alerts_green);
+		    }else{
+		    	((ImageView)view.findViewById(R.id.imageAlerts)).setImageResource(R.drawable.ic_alerts_red);		    	
+		    }
 		    return view;
 		}
 		
