a9de18d55c5dfcd85885e04f37e88f483e50da23
Art <artem@wellnuts.cz>
2014-08-22 21:02:40 +0300

    NullpointerException on tab switch from map tab fix

--- src/com/modusgo/ubi/DriverDetailsFragment.java
+++ src/com/modusgo/ubi/DriverDetailsFragment.java
@@ -20,7 +20,6 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -120,11 +119,15 @@ public class DriverDetailsFragment extends Fragment {
 		// Do a null check to confirm that we have not already instantiated the map.
 	    if (mMap == null) {
 	        // Try to obtain the map from the SupportMapFragment.
-	        mMap = ((SupportMapFragment) getActivity().getSupportFragmentManager()
-	                .findFragmentById(R.id.map)).getMap();
-	        // Check if we were successful in obtaining the map.
-	        if (mMap != null)
-	            setUpMap();
+	    	try {
+	    		mMap = ((SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+		        // Check if we were successful in obtaining the map.
+		        if (mMap != null)
+		            setUpMap();
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+			}
+	        
 	    }
     }
 
