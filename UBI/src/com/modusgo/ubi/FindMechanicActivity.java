cf5741775e57942cb5efaf76c9e59d03514912b5
Art <artem@wellnuts.cz>
2014-09-15 18:47:37 +0300

    Find mechanic API implemented

--- UBI/src/com/modusgo/ubi/FindMechanicActivity.java
+++ UBI/src/com/modusgo/ubi/FindMechanicActivity.java
@@ -1,10 +1,9 @@
 package com.modusgo.ubi;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,8 +13,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.location.Location;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -27,6 +26,12 @@ import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -40,7 +45,8 @@ import com.google.android.gms.maps.model.LatLngBounds.Builder;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.ui.IconGenerator;
 
-public class FindMechanicActivity extends MainActivity {
+public class FindMechanicActivity extends MainActivity implements ConnectionCallbacks,
+OnConnectionFailedListener, LocationListener{
 	
 	public static final String EXTRA_TRIP_ID = "tripId";
 	
@@ -51,12 +57,20 @@ public class FindMechanicActivity extends MainActivity {
 	MapView mapView;
     GoogleMap map;
     
-    LinearLayout llContent;
     ListView listView;
     LinearLayout llProgress;
     
     ArrayList<MechanicInfo> mechanics;
     MechanicsAdapter adapter;
+    
+    private LocationClient mLocationClient;
+
+    // These settings are the same as the settings for the map. They will in fact give you updates
+    // at the maximal rates currently possible.
+    private static final LocationRequest REQUEST = LocationRequest.create()
+            .setInterval(5000)         // 5 seconds
+            .setFastestInterval(3000)    // every 3 seconds
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -76,7 +90,6 @@ public class FindMechanicActivity extends MainActivity {
 		//driver = dHelper.getDriverByIndex(driverIndex);
 
 		llProgress = (LinearLayout) findViewById(R.id.llProgress);
-		llContent = (LinearLayout) findViewById(R.id.llContent);
 		listView = (ListView) findViewById(R.id.listView);
 		
 		// Gets the MapView from the XML layout and creates it
@@ -104,7 +117,11 @@ public class FindMechanicActivity extends MainActivity {
         adapter = new MechanicsAdapter(this, R.layout.find_mechanic_item, mechanics);
         listView.setAdapter(adapter);
 
-		new ReadJSONTask().execute();
+        llProgress.setVisibility(View.VISIBLE);
+        listView.setVisibility(View.GONE);
+        
+        setUpLocationClientIfNeeded();
+        mLocationClient.connect();
 	}
 	
 	private void updateMap(){
@@ -146,9 +163,59 @@ public class FindMechanicActivity extends MainActivity {
         mapView.onLowMemory();
     }
     
+    private void setUpLocationClientIfNeeded() {
+        if (mLocationClient == null) {
+            mLocationClient = new LocationClient(
+                    getApplicationContext(),
+                    this,  // ConnectionCallbacks
+                    this); // OnConnectionFailedListener
+        }
+    }
+    
+    @Override
+    public void onLocationChanged(Location location) {
+		
+    	new GetMechanicsTask(this, location.getLatitude(), location.getLongitude()).execute("find_mechanic.json");
+
+        if (mLocationClient != null) {
+            mLocationClient.disconnect();
+        }
+    }
+	
+	@Override
+    public void onConnected(Bundle connectionHint) {
+        mLocationClient.requestLocationUpdates(
+                REQUEST,
+                this);  // LocationListener
+    }
+	
+	/**
+     * Callback called when disconnected from GCore. Implementation of {@link ConnectionCallbacks}.
+     */
+    @Override
+    public void onDisconnected() {
+        // Do nothing
+    }
+
+    /**
+     * Implementation of {@link OnConnectionFailedListener}.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+    	//Do nothing
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mLocationClient != null) {
+            mLocationClient.disconnect();
+        }
+    }
+    
     class MechanicInfo{
     	
-    	float raiting;
+    	float rating;
         int reviewsCount;
         String title;
         String phoneNumber;
@@ -157,10 +224,10 @@ public class FindMechanicActivity extends MainActivity {
         LatLng coordinate;
         Bitmap icon;
         
-		public MechanicInfo(float raiting, int reviewsCount, String title,
+		public MechanicInfo(float rating, int reviewsCount, String title,
 				String phoneNumber, int index, String address, LatLng coordinate, Bitmap icon) {
 			super();
-			this.raiting = raiting;
+			this.rating = rating;
 			this.reviewsCount = reviewsCount;
 			this.title = title;
 			this.phoneNumber = phoneNumber;
@@ -203,8 +270,12 @@ public class FindMechanicActivity extends MainActivity {
 			holder.tvTitle.setText(""+mi.title);
 			holder.tvReviewCount.setText(""+mi.reviewsCount);
 			holder.tvAddress.setText(""+mi.address);
-			holder.tvPhone.setText(""+mi.phoneNumber);
-			holder.rating.setRating(mi.raiting);
+			if(!mi.phoneNumber.equals(""))
+				holder.tvPhone.setText(""+mi.phoneNumber);
+			else{
+				((View)holder.tvPhone.getParent()).setVisibility(View.GONE);
+			}
+			holder.rating.setRating(mi.rating);
 			
 			((View)holder.tvAddress.getParent()).setOnClickListener(new OnClickListener() {
 				@Override
@@ -237,66 +308,48 @@ public class FindMechanicActivity extends MainActivity {
     	RatingBar rating;
     }
     
-    class ReadJSONTask extends AsyncTask<Void, Void, Void>{
+    class GetMechanicsTask extends BaseRequestAsyncTask{
+
+		public GetMechanicsTask(Context context, double lat, double lng) {
+			super(context);
+	        requestParams.add(new BasicNameValuePair("latitude", ""+lat));
+	        requestParams.add(new BasicNameValuePair("longitude", ""+lng));
+		}
 		
 		@Override
-		protected void onPreExecute() {
-			llProgress.setVisibility(View.VISIBLE);
-			llContent.setVisibility(View.GONE);
-			super.onPreExecute();
-		}
+		protected void onSuccess(JSONObject responseJSON) throws JSONException {
+			JSONArray mechanicsJSON = responseJSON.getJSONArray("results");
+        	mechanics.clear();
+        	
+            IconGenerator iconFactory = new IconGenerator(FindMechanicActivity.this);
+            iconFactory.setBackground(getResources().getDrawable(R.drawable.marker_for_number));
+            Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/EncodeSansNormal-300-Light.ttf");
+            
+        	for (int i = 0; i < mechanicsJSON.length(); i++) {
 
-		@Override
-		protected Void doInBackground(Void... params) {
-			String json = null;
-	        try {
-
-	            InputStream is = getAssets().open("mechanics.json");
-	            int size = is.available();
-	            byte[] buffer = new byte[size];
-	            is.read(buffer);
-	            is.close();
-	            json = new String(buffer, "UTF-8");
-
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	        try{
-	        	JSONArray mechanicsJSON = new JSONArray(json);
-	        	mechanics.clear();
-	        	
-	            IconGenerator iconFactory = new IconGenerator(FindMechanicActivity.this);
-	            iconFactory.setBackground(getResources().getDrawable(R.drawable.marker_for_number));
-	            Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/EncodeSansNormal-300-Light.ttf");
-	            
-	        	for (int i = 0; i < mechanicsJSON.length(); i++) {
-
-	        		JSONObject mJSON = mechanicsJSON.getJSONObject(i);
-		        	mechanics.add(new MechanicInfo(
-		        			(float) mJSON.getDouble("raiting"), 
-		        			mJSON.getInt("reviewsCount"), 
-		        			mJSON.getString("title"), 
-		        			mJSON.getString("phoneNumber"), 
-		        			mJSON.getInt("index"), 
-		        			mJSON.getString("adress"), 
-		        			new LatLng(mJSON.getJSONArray("coordinate").getDouble(0), mJSON.getJSONArray("coordinate").getDouble(1)), 
-		        			iconFactory.makeIcon(""+mJSON.getInt("index"), 10, Color.WHITE, typeface)));
-				}
-	        	
-	        	
-	        }
-	        catch(JSONException e){
-	        	e.printStackTrace();
-	        }
+        		JSONObject mJSON = mechanicsJSON.getJSONObject(i);
+	        	mechanics.add(new MechanicInfo(
+	        			(float) mJSON.optDouble("rating"), 
+	        			mJSON.optInt("reviewsCount"), 
+	        			mJSON.optString("name"), 
+	        			mJSON.optString("phoneNumber"), 
+	        			i+1, 
+	        			mJSON.optString("vicinity"), 
+	        			new LatLng(mJSON.optDouble("lat"), mJSON.optDouble("lng")), 
+	        			iconFactory.makeIcon(""+(i+1), 10, Color.WHITE, typeface)));
+			}
 			
-			return null;
+        	if(map!=null)
+        		updateMap();
+        	
+			super.onSuccess(responseJSON);
 		}
 		
 		@Override
-		protected void onPostExecute(Void result) {
+		protected void onPostExecute(JSONObject result) {
 			super.onPostExecute(result);
 			llProgress.setVisibility(View.GONE);
-			llContent.setVisibility(View.VISIBLE);
+			listView.setVisibility(View.VISIBLE);
 			adapter.notifyDataSetChanged();
 		}
 	}
