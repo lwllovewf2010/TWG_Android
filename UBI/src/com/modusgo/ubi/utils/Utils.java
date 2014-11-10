7c4ae94795aee1668ba2096203a5680a81d35f2b
Art <artem@wellnuts.cz>
2014-11-10 18:10:16 +0300

    Utils little refactor

--- UBI/src/com/modusgo/ubi/utils/Utils.java
+++ UBI/src/com/modusgo/ubi/utils/Utils.java
@@ -186,13 +186,13 @@ public class Utils {
     }
 	
 	public static void enableDisableViewGroup(ViewGroup viewGroup, boolean enabled) {
-	    int childCount = viewGroup.getChildCount();
-	    for (int i = 0; i < childCount; i++) {
-	      View view = viewGroup.getChildAt(i);
-	      view.setEnabled(enabled);
-	      if (view instanceof ViewGroup) {
-	        enableDisableViewGroup((ViewGroup) view, enabled);
-	      }
-	    }
-	  }
+		int childCount = viewGroup.getChildCount();
+		for (int i = 0; i < childCount; i++) {
+			View view = viewGroup.getChildAt(i);
+			view.setEnabled(enabled);
+			if (view instanceof ViewGroup) {
+				enableDisableViewGroup((ViewGroup) view, enabled);
+			}
+		}
+	}
 }
